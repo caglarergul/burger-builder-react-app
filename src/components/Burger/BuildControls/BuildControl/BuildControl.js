fe072dd77fe241dafd90887c4f7e95a28a871f42
Çağlar ERGÜL <caglarergul@me.com>
2017-11-30 19:54:26 +0300

    Added BuildComponent...

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,15 @@
+/**
+ * Created by caglarergul on 30.11.2017.
+ */
+import React from 'react';
+import classes from './BuildControl.css';
+
+const buildControl = (props) => {
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
+        <button className={classes.Less}>Less</button>
+        <button className={classes.More}>More</button>
+    </div>
+};
+
+export default buildControl;
---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,13 @@
+/**
+ * Created by caglarergul on 30.11.2017.
+ */
+import React from 'react';
+import classes from './BuildControls.css';
+
+const buildControls = (props) => {
+    <div>
+
+    </div>
+};
+
+export default buildControls;
