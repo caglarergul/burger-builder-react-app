99698fe193ecc13188247c5731c376c840369d92
Çağlar ERGÜL <caglarergul@me.com>
2017-11-30 19:41:18 +0300

    Contact Arrays and message to user please add smth. in burger.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,13 +7,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
 
+    //console.log(transformedIngredients);
+    if (transformedIngredients.length ===0) {
+        transformedIngredients = <p>Please Start Adding Ingredients...</p>
+    }
     return (
         <div className={classes.Burger}>
         <BurgerIngredient type="bread-top"/>
