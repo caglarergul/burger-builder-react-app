7bb8fa1aa3c491603bb6a274425a8f2ff3b1d2e0
Caglar Ergul <caglarergul@me.com>
2017-12-04 15:48:31 +0300

    Improving the application.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@
                                              888P"
 */
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
   render() {
