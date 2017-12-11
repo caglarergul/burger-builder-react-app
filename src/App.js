7bc62bbd1bba5a1f6efd4ed2d280bfc41311f247
Caglar Ergul <caglarergul@me.com>
2017-12-11 15:17:06 +0300

    Finishing up error handlers and axios interceptors.

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
+
+
   render() {
     return (
       <div>
