f849ddcf5c7b5304cd4c7f888392d17b29357bc7
Çağlar ERGÜL <caglarergul@me.com>
2018-02-09 17:41:19 +0300

    no message

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,22 +5,26 @@ import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
-        ingredients : {
-            salad:1,
-            cheese:1,
-            meat:2,
-            bacon:3
-        }
+        ingredients : null,
+        price: 0
     };
 
-    componentDidMount() {
+    componentWillMount() {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
+        let price = 0;
         for (let param of query.entries()) {
             // ['salad', '1']
-            ingredients[param[0]] = +param[1];
+            if(param[0] === 'price') {
+                price = param[1];
+            }
+            else {
+                ingredients[param[0]] = +param[1];
+            }
+
+
         }
-        this.setState({ingredients: ingredients});
+        this.setState({ingredients: ingredients, totalPrice: price});
     }
     checkoutCancelled = () => {
 
@@ -39,7 +43,7 @@ class Checkout extends Component {
                 <CheckoutSummary ingredients={this.state.ingredients}
                 checkoutCancelled={this.checkoutCancelled}
                 checkoutContinued={this.checkoutContinued} />
-                <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData} render={() => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice}/>)}/>
             </div>
         )
     }
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
 
@@ -10,22 +11,51 @@ class ContactData extends Component {
         address: {
             street: '',
             postalCode: ''
-        }
-    }
+        },
+        loading: false
+    };
 
-    render () {
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState({loading: true});
+        const order = {
+            caglar: 'deneme',
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: "Çağlar ERGÜL",
+                address: {
+                    street: "Ankara Aslfaltı cad.",
+                    zipCode: "35000",
+                    country: "Turkey"
+                },
+                email: "caglarergul@me.com"
+            },
+            deliveryMethod: "fastest"
+        };
+
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({loading: false});
+            })
+            .catch(error => {
+                this.setState({loading: false});
+            });
+    };
+
+    render() {
         return (
             <div className={classes.ContactData}>
                 <h4>
                     Enter Your Contact Data
                 </h4>
                 <form>
-                    <input type="text" name="name" placeholder="Your name" />
-                    <input type="email" name="email" placeholder="Your email" />
-                    <input type="text" name="street" placeholder="Street" />
-                    <input type="text" name="postal" placeholder="Postal code" />
-                    <br />
-                    <Button btnType="Success">ORDER</Button>
+                    <input type="text" name="name" placeholder="Your name"/>
+                    <input type="email" name="email" placeholder="Your email"/>
+                    <input type="text" name="street" placeholder="Street"/>
+                    <input type="text" name="postal" placeholder="Postal code"/>
+                    <br/>
+                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
                 </form>
             </div>
         )
