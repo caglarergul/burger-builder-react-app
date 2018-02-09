9832e1242b05b99ef2663007eacff4d40f5b1320
Çağlar ERGÜL <caglarergul@me.com>
2018-02-09 16:56:35 +0300

    Checkout Page is created and configured.Get ingredients dynamically from BurgerBuilder Component to Checkout page.

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -11,6 +11,15 @@ class Checkout extends Component {
         }
     };
 
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for (let param of query.entries()) {
+            // ['salad', '1']
+            ingredients[param[0]] = +param[1];
+        }
+        this.setState({ingredients: ingredients});
+    }
     checkoutCancelled = () => {
 
         this.props.history.goBack();
