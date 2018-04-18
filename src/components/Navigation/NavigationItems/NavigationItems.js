3d14102ee89a7d2a292b0a03dfd2e6cd8f223559
Çağlar ERGÜL <caglarergul@me.com>
2018-04-18 15:40:06 +0300

    Fetch Orders to the Orders page.

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,11 +3,10 @@ import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => (
-    <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" active>Burger Builder</NavigationItem>
-        <NavigationItem link="/">Checkout</NavigationItem>
-    </ul>
+const navigationItems = () => ( <
+    ul className = { classes.NavigationItems } >
+    <NavigationItem link="/" exact> Burger Builder </NavigationItem>
+    <NavigationItem link="/orders"> Orders </NavigationItem> </ul>
 );
 
 export default navigationItems;
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import classes from './Order.css';
-const order = (props) => (
+const order = (props) => {
+  const ingredients=[];
+  for(let ingredientName in props.ingredients) {
+    ingredients.push(
+      {
+        name : ingredientName,
+        amount : props.ingredients[ingredientName]
+      }
+    );
+  }
+
+  const ingredientOutput = ingredients.map(ig=> {
+    return <span style={{textTransform: 'capitalize',
+          display:'inline-block',
+          margin: '0 8px',
+          border: '1px solid #cccccc',
+          padding: '5px'
+        }} key={ig.name}>{ig.name} ({ig.amount})</span>;
+  })
+  return (
   <div className={classes.Order}>
-    <p>Ingredients: Salad(1)</p>
-    <p>Price: <b>USD $4</b>  </p>
+    <p>Ingredients: {ingredientOutput}</p>
+    <p>Price: <b>USD ${Number.parseFloat(props.price).toFixed(2)}</b>  </p>
   </div>
-);
+)};
 
 export default order
