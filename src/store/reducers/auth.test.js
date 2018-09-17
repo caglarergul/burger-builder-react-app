adb3f6346ed3021b307f132bad6178f2c82ed8b0
Çağlar ERGÜL <caglarergul@me.com>
2018-09-17 19:10:40 +0300

    Testing Redux reducers & actions with Enzyme and Jest.

--- src/store/reducers/auth.test.js
+++ src/store/reducers/auth.test.js
@@ -11,5 +11,27 @@ describe('auth reducer', () => {
            authRedirectPath: '/'
        }
    )
+   });
+
+   it('should store the token upon login', () => {
+       expect(reducer({
+           token: null,
+           userId: null,
+           error: null,
+           loading: false,
+           authRedirectPath: '/'
+       },
+           {
+               type: actionTypes.AUTH_SUCCESS,
+               idToken: 'some-token',
+               userId: 'some-user-id'
+           })).toEqual(
+               {
+                token: 'some-token',
+                userId:  'some-user-id',
+                error: null,
+                loading: false,
+                authRedirectPath: '/'
+       })
    })
 });
